b6b5656516923b74ae8326f7f9b13f645ca859e3
David Gonzalez <david@novoda.com>
2014-06-18 10:42:53 +0200

    initial commit

---
+++ app/src/main/java/com/malmstein/uglyforms/DemoActivity.java
@@ -0,0 +1,15 @@
+package com.malmstein.uglyforms;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DemoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ugly_form);
+
+    }
+
+}
