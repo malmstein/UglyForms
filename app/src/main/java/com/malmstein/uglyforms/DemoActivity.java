67a6a6b2b5d84ecddd1ff048f8f082a08ea3a4f3
David Gonzalez <david@novoda.com>
2014-07-06 14:31:19 +0200

    navigation through the form with clicks

--- app/src/main/java/com/malmstein/uglyforms/DemoActivity.java
+++ app/src/main/java/com/malmstein/uglyforms/DemoActivity.java
@@ -2,14 +2,46 @@ package com.malmstein.uglyforms;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ViewAnimator;
 
-public class DemoActivity extends Activity {
+public class DemoActivity extends Activity implements View.OnClickListener {
+
+    private ViewAnimator formAnimator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ugly_form);
 
+        findViews();
     }
 
+    private void findViews(){
+        formAnimator = (ViewAnimator) findViewById(R.id.form_animator);
+        findViewById(R.id.welcome_login).setOnClickListener(this);
+        findViewById(R.id.welcome_register).setOnClickListener(this);
+        findViewById(R.id.register_cancel).setOnClickListener(this);
+        findViewById(R.id.login_cancel).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.welcome_login:
+                formAnimator.showNext();
+                break;
+            case R.id.welcome_register:
+                formAnimator.showPrevious();
+                break;
+            case R.id.register_cancel:
+                formAnimator.showNext();
+                break;
+            case R.id.login_cancel:
+                formAnimator.showPrevious();
+                break;
+            default:
+                new Exception("This view does not exist");
+        }
+    }
 }
